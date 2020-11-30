d68098401f5a63e7304e07942acad086512b93da
Kota Kanazawa <kota0818@gmail.com>
2020-11-30 20:30:14 +0900

    initial commit

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  default from: 'hoge@gmail.com'
+
+  def send_message_to_user(user)
+    @user = user
+    mail to: @user.email,
+         subject: 'メールの件名が入ります'
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,15 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener_web
+  
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "kota0818@gmail.com", #gmailアドレス
+    :password => "gaku1007kota", #gmailパスワード
+    :authentication => 'login'
+  }
 end
